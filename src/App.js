292fb17dd305549138512edbd62ba5a22d896285
ClaudiaPinaC <claudiapinac524@gmail.com>
2025-06-11 23:26:25 -0700

    RWA: env file, semantic and moment library added and new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import ExampleComponent from './components/ExampleComponent';
+import React, { useEffect, useState } from 'react';
+import Weather from './components/WeatherApp';
 
 function App() {
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                setLat(position.coords.latitude);
+                setLong(position.coords.longitude);
+            });
+
+            await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+                .then(res => res.json())
+                .then(data => {
+                    setData(data);
+                    console.log(data);
+                })
+                .catch(error => console.error('Error fetching weather data:', error));
+        }
+        fetchData();
+    }, [lat, long]);
+
     return (
-        <div>
-            <h1>Welcome to My React App</h1>
-            <ExampleComponent />
+        <div className='App'>
+            {(typeof data.main !== "undefined") ? (
+                <Weather weatherData={data} />
+            ) : (
+                <div></div>
+            )}
         </div>
     );
 }
